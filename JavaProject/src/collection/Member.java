5923f14f77f248a51fe3c718dc345bbd6e5f8e01
Hanif <marbling1293@naver.com>
2020-03-13 20:24:59 +0900

    JavaProject

---
+++ JavaProject/src/collection/Member.java
@@ -0,0 +1,31 @@
+package collection;
+
+public class Member {
+	private int memberId;		//회원 아이디
+	private String memberName;	//회원 이름
+	
+	public Member(int memberId, String memberName) {
+		this.memberId = memberId;
+		this.memberName = memberName;
+	}
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	
+	public String getMemberName() {
+		return memberName;
+	}
+	
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	
+	public String toString() {
+		return memberName + "회원님의 아이디는 " + memberId + "입니다.";
+	}
+}
