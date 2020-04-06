9376f5592299b35b44ecf984e61e1e12bcfbc360
Hanif <marbling1293@naver.com>
2020-04-06 21:41:19 +0900

    Exception Handling
    Postpone Exception Handling

---
+++ Chapter14/src/exception/ThrowsException.java
@@ -0,0 +1,25 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+	public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(fileName);	//FileNotFoundException 발생 가능
+		Class c = Class.forName(className);						//ClassNotFoundException 발생 가능
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		ThrowsException test = new ThrowsException();
+		try {
+			test.loadClass("a.text", "java.lang.String");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {		//Exception 클래스로 그 외 예외 상황 처리
+		e.printStackTrace();
+		}
+	}
+}
